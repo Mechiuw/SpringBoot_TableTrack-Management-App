aba74ddd7e7083c98a61ccf5d752f1cff7de43b9
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-07 00:58:41 +0700

    [IMPROVE] Improved create method in OrderServiceImpl, on progress for transactions

--- src/main/java/com/bahari/bahari_resto_API/service/impl/OrderServiceImpl.java
+++ src/main/java/com/bahari/bahari_resto_API/service/impl/OrderServiceImpl.java
@@ -2,18 +2,22 @@ package com.bahari.bahari_resto_API.service.impl;
 
 import com.bahari.bahari_resto_API.model.dto.request.OrderRequest;
 import com.bahari.bahari_resto_API.model.dto.response.OrderResponse;
+import com.bahari.bahari_resto_API.model.entity.Customer;
 import com.bahari.bahari_resto_API.model.entity.Order;
+import com.bahari.bahari_resto_API.repository.CustomerRepository;
 import com.bahari.bahari_resto_API.repository.OrderRepository;
 import com.bahari.bahari_resto_API.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
+    private final CustomerRepository customerRepository;
 
     private void validateOrderRequest(OrderRequest x){
         if(x == null){
@@ -32,9 +36,24 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public OrderResponse create(OrderRequest orderRequest) {
+        //TODO 1 : Input validation
         validateOrderRequest(orderRequest);
 
+        //TODO 2 : Retrieve Customer
+        Customer customer = customerRepository.findById(orderRequest.getCustomerId())
+                .orElseThrow(() -> new NoSuchElementException("Customer not found with id : " +
+                        orderRequest.getCustomerId()));
 
+        //TODO 3 : Create / Build Order Entity
+        Order order = Order.builder().build();
+        order.setDateTime(orderRequest.getDateTime());
+        order.setCustomerId(customer);
+
+        //TODO 4 : Set Order Details
+
+        //TODO 5 : Save Order Entity
+
+        //TODO 6 : Build Response
 
         return null;
     }
