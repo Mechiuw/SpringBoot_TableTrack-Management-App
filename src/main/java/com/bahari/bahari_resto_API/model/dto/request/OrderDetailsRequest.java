c0f5fd001b43107f09de25fd7ee265ec6bde8469
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-07 00:30:15 +0700

    [FEATURE] Added Order and OrderDetail DTO

---
+++ src/main/java/com/bahari/bahari_resto_API/model/dto/request/OrderDetailsRequest.java
@@ -0,0 +1,6 @@
+package com.bahari.bahari_resto_API.model.dto.request;
+
+public class OrderDetailsRequest {
+    private Integer quantity;
+    private String productId;
+}
---
+++ src/main/java/com/bahari/bahari_resto_API/model/dto/request/OrderRequest.java
@@ -0,0 +1,20 @@
+package com.bahari.bahari_resto_API.model.dto.request;
+
+import com.bahari.bahari_resto_API.model.entity.OrderDetails;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class OrderRequest {
+    private LocalDateTime dateTime;
+    private String customerId;
+    private List<OrderDetailsRequest> orderDetails;
+}
