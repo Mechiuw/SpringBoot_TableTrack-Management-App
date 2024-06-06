9090008b74104ab88b43ab12b0a36ab81f11ad29
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-06 22:49:53 +0700

    [FEATURE] Added create method for StoreServiceImpl

--- src/main/java/com/bahari/bahari_resto_API/service/impl/StoreServiceImpl.java
+++ src/main/java/com/bahari/bahari_resto_API/service/impl/StoreServiceImpl.java
@@ -4,6 +4,7 @@ import com.bahari.bahari_resto_API.model.dto.request.StoreRequest;
 import com.bahari.bahari_resto_API.model.dto.response.StoreResponse;
 import com.bahari.bahari_resto_API.model.entity.Store;
 import com.bahari.bahari_resto_API.repository.CustomerRepository;
+import com.bahari.bahari_resto_API.repository.StoreRepository;
 import com.bahari.bahari_resto_API.service.StoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,11 +14,22 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class StoreServiceImpl implements StoreService {
-    private final CustomerRepository customerRepository;
+    private final StoreRepository storeRepository;
 
     @Override
     public StoreResponse create(StoreRequest storeRequest) {
-        return null;
+        Store store = Store.builder()
+                .name(storeRequest.getName())
+                .address(storeRequest.getAddress())
+                .email(storeRequest.getEmail())
+                .phoneNumber(storeRequest.getPhoneNumber())
+                .build();
+        storeRepository.save(store);
+        return StoreResponse.builder()
+                .name(store.getName())
+                .address(store.getAddress())
+                .phoneNum(store.getPhoneNumber())
+                .build();
     }
 
     @Override
