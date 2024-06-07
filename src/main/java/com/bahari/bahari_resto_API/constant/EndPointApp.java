0f9f25ec22ad2c67bfe44bac2bc86f5b9982425b
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-08 01:09:28 +0700

    [FEATURE] Added Endpoint application

---
+++ src/main/java/com/bahari/bahari_resto_API/constant/EndPointApp.java
@@ -0,0 +1,11 @@
+package com.bahari.bahari_resto_API.constant;
+
+public class EndPointApp {
+    public static final String STORE = "/store";
+    public static final String CUSTOMER = "/customer";
+    public static final String PRODUCT = "/product";
+
+    public static final String GET_BY_ID = "/{id}";
+    public static final String PUT_BY_ID = "/{id}";
+    public static final String DELETE_BY_ID = "/{id}";
+}
---
+++ src/main/java/com/bahari/bahari_resto_API/controller/CustomerController.java
@@ -0,0 +1,9 @@
+package com.bahari.bahari_resto_API.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CustomerController {
+}
