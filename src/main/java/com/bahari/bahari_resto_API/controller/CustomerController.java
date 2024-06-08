c0f3c328adf56a0368d188c2def1ee3ed08ed295
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-08 15:09:26 +0700

    [IMPROVE] Improved CustomerController added anotations

--- src/main/java/com/bahari/bahari_resto_API/controller/CustomerController.java
+++ src/main/java/com/bahari/bahari_resto_API/controller/CustomerController.java
@@ -4,15 +4,15 @@ import com.bahari.bahari_resto_API.constant.EndPointApp;
 import com.bahari.bahari_resto_API.model.dto.request.CustomerRequest;
 import com.bahari.bahari_resto_API.model.dto.response.CommonResponse;
 import com.bahari.bahari_resto_API.model.dto.response.CustomerResponse;
+import com.bahari.bahari_resto_API.model.entity.Customer;
 import com.bahari.bahari_resto_API.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,5 +29,44 @@ public class CustomerController {
                         .message("Successfully created new Customer")
                         .data(customerResponse)
                         .build());
+    }
+
+    @GetMapping("/all")
+    private ResponseEntity<?> getAll(){
+        List<Customer> customerData = customerService.getAll();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(CommonResponse.builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .message("Successfully fetch data")
+                        .data(customerData));
+    }
+
+    @GetMapping(EndPointApp.GET_BY_ID)
+    private ResponseEntity<?> getById(@PathVariable String id){
+        CustomerResponse customerResponse = customerService.getId(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(CommonResponse.builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .message("Successfully fetch data")
+                        .data(customerResponse));
+    }
+
+    @PutMapping(EndPointApp.PUT_BY_ID)
+    private ResponseEntity<?> update(@PathVariable String id,@RequestBody CustomerRequest customerRequest){
+        CustomerResponse customerResponse = customerService.update(id,customerRequest);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(CommonResponse.builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .message("Successfully update data")
+                        .data(customerResponse));
+    }
+
+    @DeleteMapping(EndPointApp.DELETE_BY_ID)
+    private void delete(@PathVariable String id){
+        customerService.delete(id);
+        ResponseEntity.status(HttpStatus.OK)
+                .body(CommonResponse.builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .message("Successfully delete data"));
     }
 }
