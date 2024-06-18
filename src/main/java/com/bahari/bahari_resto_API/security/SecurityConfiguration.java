c5a230896eccf686f8ac7ab5532cb6f4a7dd8f18
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-18 10:43:41 +0700

    [IMPROVE] Improved security configuration

--- src/main/java/com/bahari/bahari_resto_API/security/SecurityConfiguration.java
+++ src/main/java/com/bahari/bahari_resto_API/security/SecurityConfiguration.java
@@ -1,6 +1,34 @@
 package com.bahari.bahari_resto_API.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.HttpSecurityDsl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-public class SecurityConfiguration extends WebSecurityConfiguration {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((request) -> request
+                .requestMatchers("/public/**").permitAll()
+                .anyRequest().authenticated())
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll())
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
