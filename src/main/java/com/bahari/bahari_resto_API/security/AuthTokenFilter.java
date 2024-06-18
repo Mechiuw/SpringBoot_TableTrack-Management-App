ad3f7ca32b557c5a761ab42c4b52faad50b21f61
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-18 20:48:46 +0700

    [WIP] Work In Progress done for AuthTokenFilter

--- src/main/java/com/bahari/bahari_resto_API/security/AuthTokenFilter.java
+++ src/main/java/com/bahari/bahari_resto_API/security/AuthTokenFilter.java
@@ -1,12 +1,54 @@
 package com.bahari.bahari_resto_API.security;
 
+import com.bahari.bahari_resto_API.service.UserService;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
 
 @RequiredArgsConstructor
 @Component
 public class AuthTokenFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
-    private final
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String headerAuth = request.getHeader(HttpHeaders.AUTHORIZATION);
+            String token = null;
+            if(headerAuth != null && headerAuth.startsWith("Bearer ")){
+                token = headerAuth.substring(7);
+            }
+            if(token != null && jwtUtil.verifyJwtToken(token)){
+                //set auth to spring security
+                Map<String, String> userInfo = jwtUtil.getUserInfoByToken(token);
+                UserDetails user = userService.loadUserByUserId(userInfo.get("userId"));
+                //token validation
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
+                //add information containing IP address to security
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource());
+
+                //saving auth to spring
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        filterChain.doFilter(request,response);
+    }
 }
