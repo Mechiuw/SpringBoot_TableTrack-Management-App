7b4d55cde4bcfbcb53b9707106690410718d97c1
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-18 20:56:48 +0700

    [FEATURE] Added BeanConfiguration files and relocated swaggerconfiguration

--- src/main/java/com/bahari/bahari_resto_API/config/SwaggerConfiguration.java
+++ src/main/java/com/bahari/bahari_resto_API/config/SwaggerConfiguration.java
@@ -1,4 +1,4 @@
-package com.bahari.bahari_resto_API.utils;
+package com.bahari.bahari_resto_API.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
