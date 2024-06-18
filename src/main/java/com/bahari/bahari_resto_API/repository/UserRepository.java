9112b6bc9f56afd79bdeba9f977e9084e68413d8
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-18 19:57:45 +0700

    [FEATURE] Added AppUser replacing UserEntity

--- src/main/java/com/bahari/bahari_resto_API/repository/UserRepository.java
+++ src/main/java/com/bahari/bahari_resto_API/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.bahari.bahari_resto_API.repository;
 
-import com.bahari.bahari_resto_API.model.entity.UserEntity;
+import com.bahari.bahari_resto_API.model.entity.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity,String> {
+public interface UserRepository extends JpaRepository<AppUser,String> {
 }
