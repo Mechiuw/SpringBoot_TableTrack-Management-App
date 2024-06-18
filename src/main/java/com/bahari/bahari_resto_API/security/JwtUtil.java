7c8cc08df7e060a4b8895c9c9d9042decf734c7e
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-18 19:37:27 +0700

    [WIP] Work in progress with jwt utility

--- src/main/java/com/bahari/bahari_resto_API/security/JwtUtil.java
+++ src/main/java/com/bahari/bahari_resto_API/security/JwtUtil.java
@@ -1,4 +1,22 @@
 package com.bahari.bahari_resto_API.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtil {
+    @Value("${app.shopee.jwt.jwt-secret")
+    private String jwtSecret;
+
+    @Value("${app.shopee.jwt.app-name")
+    private String appName;
+
+    @Value("${app.shopee.jwt.jwt-expiert")
+    private long jwtExpiration;
+
+    //TODO 1 : Generate tokens
+    public String generateToken(AppUser appUser){
+
+    }
+
 }
