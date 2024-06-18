81a754bba750ec900d6bd586b3384a868550cbf3
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-18 20:08:22 +0700

    [FEATURE] Added VerifyJWTToken function to verify tokens

--- src/main/java/com/bahari/bahari_resto_API/security/JwtUtil.java
+++ src/main/java/com/bahari/bahari_resto_API/security/JwtUtil.java
@@ -1,8 +1,11 @@
 package com.bahari.bahari_resto_API.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.bahari.bahari_resto_API.model.entity.AppUser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -21,7 +24,7 @@ public class JwtUtil {
     @Value("${app.shopee.jwt.jwt-expiert")
     private long jwtExpiration;
 
-    //TODO 1 : Generate tokens
+    //TODO 1 : Make/Generate new token
     public String generateToken(AppUser appUser){
         try {
             Algorithm algorithm = Algorithm.HMAC256(jwtSecret.getBytes(StandardCharsets.UTF_8));
@@ -34,6 +37,18 @@ public class JwtUtil {
                     .sign(algorithm);
             return token;
         } catch (JWTCreationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //TODO 2 : Verify the token we just made
+    public boolean verifyJwtToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(jwtSecret.getBytes(StandardCharsets.UTF_8));
+            JWTVerifier verifier = JWT.require(algorithm).build();
+            DecodedJWT decodedJWT = verifier.verify(token);
+            return decodedJWT.getIssuer().equals(appName);
+        } catch (JWTVerificationException e){
             throw new RuntimeException(e);
         }
     }
