fc472249a9e29b40d28a9832c400aa4b29da9959
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-18 08:15:47 +0700

    [WIP] Spring security progress

---
+++ src/main/java/com/bahari/bahari_resto_API/security/SecurityConfiguration.java
@@ -0,0 +1,6 @@
+package com.bahari.bahari_resto_API.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfiguration extends WebSecurityConfiguration {
+}
