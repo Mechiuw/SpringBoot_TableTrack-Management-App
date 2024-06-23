bfc77dbb7e98a1cd33f530ff6cd9bc2008c41130
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-23 18:56:54 +0700

    [FEATURE] Added Create function for import

--- src/main/java/com/tabletrack/table_track_API/controller/ImportController.java
+++ src/main/java/com/tabletrack/table_track_API/controller/ImportController.java
@@ -1,10 +1,34 @@
 package com.tabletrack.table_track_API.controller;
 
 import com.tabletrack.table_track_API.constant.EndPointApp;
+import com.tabletrack.table_track_API.model.dto.request.ImportRequest;
+import com.tabletrack.table_track_API.model.dto.response.CommonResponse;
+import com.tabletrack.table_track_API.model.dto.response.ImportResponse;
+import com.tabletrack.table_track_API.service.ImportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(EndPointApp.IMPORT)
+@RequiredArgsConstructor
 public class ImportController {
+    private final ImportService importService;
+    public ResponseEntity<?> create(ImportRequest importRequest){
+        ImportResponse importResponse = importService.create(importRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                CommonResponse.builder()
+                        .statusCode(HttpStatus.OK.value())
+                        .message("Successfully created import transaction")
+                        .data(importResponse)
+                        .build()
+        );
+    }
+    public ResponseEntity<?> getById(String id){}
+    public ResponseEntity<?> getAll(){}
+    public ResponseEntity<?> update(String id, ImportRequest importRequest){}
+    public void delete(ImportRequest importRequest){}
 }
