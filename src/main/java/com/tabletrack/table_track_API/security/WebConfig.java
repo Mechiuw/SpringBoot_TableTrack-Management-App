0e4093c842a89956f6abe8bfff5edc0cd06ca37d
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-01 18:22:51 +0700

    [FEATURE] Web Configurations

--- src/main/java/com/tabletrack/table_track_API/security/WebConfig.java
+++ src/main/java/com/tabletrack/table_track_API/security/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("api/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("POST","GET","PUT","DELETE")
                 .allowedHeaders("*")
